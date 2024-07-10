82681a501de79fdf4228419d2978fdc2713baf1e
Jeffiros <krittiwat.c@gmail.com>
2024-07-10 14:22:28 +0700

    add answers

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,17 @@
 function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let result = [];
+  for (let num in numbers) {
+    for (let no in numbers) {
+      if (no != num) {
+        if (numbers[num] + numbers[no] === target) {
+          result.push(num);
+        }
+      }
+    }
+  }
+  return result;
+}
+
+numbers = [2, 7, 11, 15];
+console.log(twoSum(numbers, 9));
